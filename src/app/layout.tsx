448ae1c252369b18997ff215dab3e79a764d901e
Sachin <mittalsam98@gmail.com>
2023-09-19 13:18:48 +0530

    Added Metadata and favicon icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,13 @@ import { ClerkProvider } from '@clerk/nextjs';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import RecoilContextProvider from './RecoilContextProvider';
+import type { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
+export const metadata: Metadata = {
+  title: 'ChatGPT Clone',
+  description: 'ChatGPT clone with simple design'
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
